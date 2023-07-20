c5664a4996c77070f773658dc093bf52f1784f39
Chuchundra009 <redfox161294@gmail.com>
2023-07-20 23:32:06 +0300

    block 4 5 6

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
 const prev = document.querySelector("#prev");
 const next = document.querySelector("#next");
 
+const productContainers = [...document.querySelectorAll('.client__reviews-container')];
+const nxtBtn = [...document.querySelectorAll(".client__reviews-nxt")];
+const preBtn = [...document.querySelectorAll(".client__reviews-pre")];
+
 const photos = ["./assets/img/order_images/order-furniture-one.jpg", "./assets/img/order_images/order-furniture-two.jpg", "./assets/img/order_images/order-furniture-three.jpg", "./assets/img/order_images/order-furniture-three.png", "./assets/img/order_images/order-furniture-four.png", "./assets/img/order_images/order-furniture-five.png", "./assets/img/order_images/order-furniture-six.png", "./assets/img/order_images/order-furniture-seven.png", "./assets/img/order_images/order-furniture-eight.png", "./assets/img/order_images/order-furniture-nine.png", "./assets/img/order_images/order-furniture-ten.png", "./assets/img/order_images/order-furniture-eleven.png"];
 
 let i = 0;
@@ -21,6 +25,24 @@ prev.addEventListener("click", () => {
         i= photos.length - 1;
     }
     document.querySelector(".portfolio__photo-img").src = photos[i];
+})
+
+
+
+
+
+
+productContainers.forEach((item, i) => {
+    let containerDimensions = item.getBoundingClientRect();
+    let containerWidth = containerDimensions.width;
+
+    nxtBtn[i].addEventListener('click', () => {
+        item.scrollLeft += containerWidth;
+    })
+
+    preBtn[i].addEventListener('click', () => {
+        item.scrollLeft -= containerWidth;
+    })
 })
 
 
